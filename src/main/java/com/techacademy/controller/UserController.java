a8c37ded881349ce2eb995c238d501e051a18f5f
kenchanta <exnowman@gmail.com>
2025-03-29 19:28:23 +0900

    Lesson18 Chapter5「User一覧表示機能の作成」

---
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.techacademy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.techacademy.service.UserService;
+
+@Controller
+@RequestMapping("user") /*クラス全体に対して /user という部分のURLを追加することを意味する*/
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    /** 一覧画面を表示 */
+    @GetMapping("/list") /*("/list)へのGETリクエストを処理するメソッドを指定。@GetMapping("/user/list") と書くと、二重のuserがURLに含まれてしまう（/user/user/list）ので避ける。*/
+    public String getList(Model model) {
+        model.addAttribute("userlist", service.getUserList());
+        return "user/list";
+    }
+}
+
+
